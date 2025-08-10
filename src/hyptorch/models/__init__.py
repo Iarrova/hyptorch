01ac7fa5426164d6f8923c1b60785abaaf666368
Ian Roberts <iroberts1398@gmail.com>
2025-08-10 14:11:28 -0400

    Update manifolds to models and delete repeated documentation

---
+++ src/hyptorch/models/__init__.py
@@ -0,0 +1,4 @@
+from hyptorch.models.poincare_ball import PoincareBall
+from hyptorch.models.transformations import KleinToPoincareTransform, PoincareToKleinTransform
+
+__all__ = ["PoincareBall", "KleinToPoincareTransform", "PoincareToKleinTransform"]
